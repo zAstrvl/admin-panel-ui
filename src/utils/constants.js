8bcfe681a6afff6984a977d2c71012d03c43911c
zAstrvl <xrean.aep@gmail.com>
2025-07-26 19:24:31 +0300

    added url for azure

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-//export const apiUrl = "https://school-grdwdcdgbkesdtcf.germanywestcentral-01.azurewebsites.net";
+export const apiUrl = "https://school-grdwdcdgbkesdtcf.germanywestcentral-01.azurewebsites.net";
 
-export const apiUrl = "https://localhost:7294";
+//export const apiUrl = "https://localhost:7294";
